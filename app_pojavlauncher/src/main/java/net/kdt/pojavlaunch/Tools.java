f5fefe45e3c0030fa9166909071f2b897b62a4fc
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-15 11:10:24 +0800

    Update Tools.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
@@ -1476,7 +1476,7 @@ public final class Tools {
 
     public static boolean isLocalProfile(Context ctx){
         MinecraftAccount currentProfile = PojavProfile.getCurrentProfileContent(ctx, null);
-        return currentProfile == null || currentProfile.isLocal();
+        return false;
     }
     public static boolean hasOnlineProfile(){
         for (MinecraftAccount accountToCheck : getAllProfiles()) {
